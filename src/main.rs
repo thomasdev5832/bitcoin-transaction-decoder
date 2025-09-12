2915086d9b56f9e46b5cae8da6106df05f47e349
thomasdev5832 <eugabrielthome@gmail.com>
2025-09-12 14:51:36 -0300

    arrays

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 #[allow(unused)]
 fn read_version(_transaction_hex: &str) -> u32 {
     let transaction_bytes = hex::decode(_transaction_hex).unwrap();
-    let version_bytes = &transaction_bytes[0..4];
-    println!("version bytes: {:?}", version_bytes);
-    1
+    let version_bytes: [u8; 4] = (&transaction_bytes[0..4]).try_into().unwrap();
+    u32::from_le_bytes(version_bytes)
 }
 
 fn main() {
