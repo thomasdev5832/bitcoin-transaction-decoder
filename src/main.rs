bdcae11c4de65e2db6e8f9293a5ca3eb681285a8
thomasdev5832 <eugabrielthome@gmail.com>
2025-09-15 10:19:17 -0300

    unit testing

--- src/main.rs
+++ src/main.rs
@@ -41,3 +41,32 @@ fn main() {
     println!("version: {}", version);
     println!("input length: {}", input_length);
 }
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_read_compact_size() {
+        let mut bytes = [1_u8].as_slice();
+        let count = super::read_compact_size(&mut bytes);
+        assert_eq!(count, 1_u64);
+
+        let mut bytes = [253_u8, 0, 1].as_slice();
+        let count = super::read_compact_size(&mut bytes);
+        assert_eq!(count, 256_u64);
+
+        let mut bytes = [254_u8, 0, 0, 0, 1].as_slice();
+        let count = super::read_compact_size(&mut bytes);
+        assert_eq!(count, 256_u64.pow(3));
+
+        let mut bytes = [255_u8, 0, 0, 0, 0, 0, 0, 0, 1].as_slice();
+        let count = super::read_compact_size(&mut bytes);
+        assert_eq!(count, 256_u64.pow(7));
+
+        let hex = "fd204e";
+        let decoded = hex::decode(hex).unwrap();
+        let mut bytes = decoded.as_slice();
+        let count = super::read_compact_size(&mut bytes);
+        let expected_count = 20_000_u64;
+        assert_eq!(count, expected_count);
+    }
+}
